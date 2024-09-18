bbdcc9ebc86223175ccf68cc99976646ccead693
Karine Antunes de Farias <50750300+karineaf@users.noreply.github.com>
2024-09-18 20:26:06 -0300

    excluding .idea, target and .mvn

--- target/classes/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO notification (user_id, type, sent_date) VALUES (1L, 'marketing', '2024-09-13 22:15')
-INSERT INTO notification (user_id, type, sent_date)  VALUES (1L, 'marketing', '2024-09-13 22:30')
