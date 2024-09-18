ab6eff5a5470dcfc7b2f39684257e3e652908a87
Karine Antunes de Farias <50750300+karineaf@users.noreply.github.com>
2024-09-17 21:24:13 -0300

    creating a bean of notificationservice for springboot can recognise the components

--- src/main/java/com/javaproject/notificationservice/NotificationserviceApplication.java
+++ src/main/java/com/javaproject/notificationservice/NotificationserviceApplication.java
@@ -1,25 +1,25 @@
 package com.javaproject.notificationservice;
 
-import com.javaproject.notificationservice.gateway.Gateway;
-import com.javaproject.notificationservice.services.Implementation.StatusNotificationService;
 import com.javaproject.notificationservice.services.NotificationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 
 @SpringBootApplication
 public class NotificationserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotificationserviceApplication.class, args);
+	public static void main(String[] args) throws InterruptedException {
+		ApplicationContext context = SpringApplication.run(NotificationserviceApplication.class, args);
 		System.out.println("Notification service is running...");
 
-		NotificationService service = new StatusNotificationService(new Gateway());
-        
-        service.send("news", "user", "news 1");
-        service.send("news", "user", "news 2");
-        service.send("news", "user", "news 3");
-        service.send("news", "another user", "news 1");
-        service.send("update", "user", "update 1");
+		NotificationService service = context.getBean(NotificationService.class);
+		service.send("marketing", 1L, "marketing 1");
+		service.send("marketing", 1L, "marketing 2");
+		service.send("marketing", 1L, "marketing 3");
+		service.send("update", 1L, "update 1");
+        service.send("news", 1L, "news 2");
+        service.send("news", 1L, "news 3");
+        service.send("news", 2L, "news 1");
 	}
-
 }
