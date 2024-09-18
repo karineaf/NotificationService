541d3be6a87801aa184c9cf532480e4f0fb5321b
Karine Antunes de Farias <50750300+karineaf@users.noreply.github.com>
2024-09-17 21:20:08 -0300

    removing unnecessary and unused code

--- src/main/java/com/javaproject/notificationservice/utils/ConstantsUtils.java
+++ src/main/java/com/javaproject/notificationservice/utils/ConstantsUtils.java
@@ -1,19 +1,11 @@
 package com.javaproject.notificationservice.utils;
 
-import com.javaproject.notificationservice.model.Notification;
-
-import static java.time.Duration.*;
 
 public final class ConstantsUtils {
 
     private ConstantsUtils() {
         throw new AssertionError("Cannot instantiate a final class - Constants");
     }
-
-    public static final Notification MARKETING = new Notification("marketing", ofHours(1), 3);
-    public static final Notification NEWS = new Notification("news", ofDays(1), 1);
-    public static final Notification STATUS = new Notification("status", ofMinutes(1), 2);
-
 
     public static final int MARKETING_MAX_REQUESTS = 3;
     public static final int STATUS_MAX_REQUESTS = 2;
