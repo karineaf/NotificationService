1ad929db8c35655bd77248c4df123b5a9f725e73
Karine Antunes de Farias <50750300+karineaf@users.noreply.github.com>
2024-09-17 21:24:46 -0300

    changing the find method to filter by type also

--- src/test/java/com/javaproject/notificationservice/NotificationserviceApplicationTests.java
+++ src/test/java/com/javaproject/notificationservice/NotificationserviceApplicationTests.java
@@ -3,11 +3,11 @@ package com.javaproject.notificationservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class NotificationserviceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class NotificationserviceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
